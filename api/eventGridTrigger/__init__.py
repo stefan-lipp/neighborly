ffc4b923d4c768ecf02b99da6bfb9d59d31d74c3
Stefan Lipp <stefan.cimander@me.com>
2024-06-25 14:40:47 +0200

    Configure handlers for event grid topic and logic app trigger

---
+++ api/eventGridTrigger/__init__.py
@@ -0,0 +1,15 @@
+import json
+import logging
+
+from azure.functions import EventGridEvent
+
+def main(event: EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event: %s', result)
